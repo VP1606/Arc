b2226ce753732b16ed3acc6f91c2bbdc9c58128e
VP1606 <65492689+VP1606@users.noreply.github.com>
2022-10-14 20:28:27 +0100

    Packed Get Item into function.

--- bestway/get_item_sys/get_item.py
+++ bestway/get_item_sys/get_item.py
@@ -26,40 +26,42 @@ headers = {
     'Connection': 'keep-alive',
 }
 
-page = requests.get('https://www.bestwaywholesale.co.uk/product/656016-1', cookies=cookies, headers=headers)
-soup = BeautifulSoup(page.content, "html.parser")
-right_block = soup.find(id="shop-products")
 
-name = right_block.find_all("h1", class_="prodname")[0].text
-unit_size = right_block.find_all("p", class_="prodsize")[0].text
-b_price = right_block.find_all("p", class_="prodprice")[0].text
+def GET_ITEM(link_code, cookies, headers):
+    url = 'https://www.bestwaywholesale.co.uk/product/{0}'.format(link_code)
+    page = requests.get(url, cookies=cookies, headers=headers)
+    soup = BeautifulSoup(page.content, "html.parser")
+    right_block = soup.find(id="shop-products")
 
-prod_table = right_block.find_all("table", class_="prodtable")[0]
-prod_table_rows = prod_table.find_all("tr")
+    name = right_block.find_all("h1", class_="prodname")[0].text
+    unit_size = right_block.find_all("p", class_="prodsize")[0].text
+    b_price = right_block.find_all("p", class_="prodprice")[0].text
 
-rsp, por, pack_size, code, ean, vat_rate, brand = "", "", "", "", "", "", ""
+    prod_table = right_block.find_all("table", class_="prodtable")[0]
+    prod_table_rows = prod_table.find_all("tr")
 
+    rsp, por, pack_size, code, ean, vat_rate, brand = "", "", "", "", "", "", ""
 
-for row in prod_table_rows:
-    value = row.find_all("td")[0].text
-    flag = row.find_all("th")[0].text
-    if flag == "RSP:":
-        rsp = value
-    elif flag == "POR:":
-        por = value
-    elif flag == "Pack Size:":
-        pack_size = value
-    elif flag == "Product Code:":
-        code = value
-    elif flag == "Retail EAN:":
-        ean = value
-    elif flag == "Vat Rate:":
-        vat_rate = value
-    elif flag == "Brand:":
-        brand = value
-    else:
-        print("Unexpected ROW PARSE FIELD!")
-        # Error Handling? Unexpected Val Detected!
+    for row in prod_table_rows:
+        value = row.find_all("td")[0].text
+        flag = row.find_all("th")[0].text
+        if flag == "RSP:":
+            rsp = value
+        elif flag == "POR:":
+            por = value
+        elif flag == "Pack Size:":
+            pack_size = value
+        elif flag == "Product Code:":
+            code = value
+        elif flag == "Retail EAN:":
+            ean = value
+        elif flag == "Vat Rate:":
+            vat_rate = value
+        elif flag == "Brand:":
+            brand = value
+        else:
+            print("Unexpected ROW PARSE FIELD!")
+            # Error Handling? Unexpected Val Detected!
 
-item = BestwayItem(name, unit_size, b_price, rsp, por, pack_size, code, ean, vat_rate, brand)
-
+    item = BestwayItem(name, unit_size, b_price, rsp, por, pack_size, code, ean, vat_rate, brand)
+    return item
