2b593b8c9ec44cbc99a44bb8d02dca4bdf8f966b
VP1606 <65492689+VP1606@users.noreply.github.com>
2022-10-16 04:24:44 +0100

    Added progress bar.

--- bestway/get_all_cat.py
+++ bestway/get_all_cat.py
@@ -1,35 +1,49 @@
 import requests
 from bs4 import BeautifulSoup
 import get_item
+from alive_progress import alive_bar
+import math
 
 def Upload_Category(href, cookies, headers, mydb):
     target_book = []
 
+    url = 'https://www.bestwaywholesale.co.uk/{0}'.format(href)
+    page = requests.get(url, cookies=cookies, headers=headers)
+    soup = BeautifulSoup(page.content, "html.parser")
+
+    max_span_text = soup.find_all("span", class_="desktop")[0].text
+    max_val = int(max_span_text.split(" ").pop())
+    max_pages = math.ceil(max_val / 20)
+
     incrament_count = 0
     nextable_flag = True
-    while nextable_flag:
-        extension = ""
-        if incrament_count != 0:
-            extension = "?s={0}".format(str(incrament_count))
 
-        url = 'https://www.bestwaywholesale.co.uk/{0}{1}'.format(href, extension)
-        page = requests.get(url, cookies=cookies, headers=headers)
-        soup = BeautifulSoup(page.content, "html.parser")
+    with alive_bar(max_pages, title="Page Scanning", force_tty=True) as bar:
+        while nextable_flag:
+            extension = ""
+            if incrament_count != 0:
+                extension = "?s={0}".format(str(incrament_count))
 
-        the_list = soup.find(id="shop-products")
-        list_elements = the_list.find_all("li")
-        valids = 0
-        for el in list_elements:
-            try:
-                target_book.append(el["data-ga-product-id"])
-                valids += 1
-            except:
-                pass
+            url = 'https://www.bestwaywholesale.co.uk/{0}{1}'.format(href, extension)
+            page = requests.get(url, cookies=cookies, headers=headers)
+            soup = BeautifulSoup(page.content, "html.parser")
 
-        if valids < 20:
-            nextable_flag = False
-        else:
-            incrament_count += 20
+            the_list = soup.find(id="shop-products")
+            list_elements = the_list.find_all("li")
+            valids = 0
+            for el in list_elements:
+                try:
+                    target_book.append(el["data-ga-product-id"])
+                    valids += 1
+                except:
+                    pass
+
+            if valids < 20:
+                nextable_flag = False
+            else:
+                incrament_count += 20
+
+            bar()
 
     print(len(target_book))
 
