8061c161ee8945b63e9bc1d638ca9c33c061e1dd
VP1606 <65492689+VP1606@users.noreply.github.com>
2022-10-25 19:12:15 +0100

    Added profit percent metric (SQL Only).

--- bestway/bway_item.py
+++ bestway/bway_item.py
@@ -27,10 +27,29 @@ class BestwayItem:
         rsp_formatted = float(self.rsp[1:])
         b_price_formatted = float(self.b_price[1:])
 
+        broken_units = self.unit_size.split(" ")
+        broken_units.pop(0)
+
+        no_units = 1
+        for num in broken_units:
+            try:
+                no_units = no_units * int(num)
+            except:
+                pass
+
+        profit_overhead = (rsp_formatted * no_units) - b_price_formatted
+
+        if profit_overhead == 0.0 or b_price_formatted == 0.0:
+            profit_percent = 0.000
+        else:
+            profit_percent = (profit_overhead / b_price_formatted) * 100
+
+        profit_percent = round(profit_percent, 2)
+
         mycursor = mydb.cursor()
 
-        sql = "INSERT INTO rrpextract (datetime, stockref, description, price, source, vat, por, packsize, 	suppliercode, brand, in_stock, supplier_price, supplier_packsize) VALUES (NOW(), %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
-        vals = (self.ean, self.name, rsp_formatted, "BESTWAY", vat_val, self.por, self.pack_size, int(self.code), self.brand, self.stock, b_price_formatted, self.unit_size)
+        sql = "INSERT INTO rrpextract (datetime, stockref, description, price, source, vat, por, packsize, 	suppliercode, brand, in_stock, supplier_price, supplier_packsize, profit_percent) VALUES (NOW(), %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+        vals = (self.ean, self.name, rsp_formatted, "BESTWAY", vat_val, self.por, self.pack_size, int(self.code), self.brand, self.stock, b_price_formatted, self.unit_size, profit_percent)
 
         mycursor.execute(sql, vals)
         mydb.commit()
