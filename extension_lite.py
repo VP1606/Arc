71fd9b377b49993e30af265e1b766708d659ca47
VP1606 @ 16MBP <vpremakantha@gmail.com>
2023-01-07 17:44:42 +0000

    Completed sql lite pack.

--- extension_lite.py
+++ extension_lite.py
@@ -1,40 +1,19 @@
 import mysql.connector
 import os
 
-# mydbs = [mysql.connector.connect(
-#         host="milecross.dyndns.org",
-#         user="mpos",
-#         password="mpospass",
-#         database="mpos"
-#     ), mysql.connector.connect(
-#         host="netherly1.dyndns.org",
-#         user="mpos",
-#         password="mpospass",
-#         database="mpos"
-#     ), mysql.connector.connect(
-#         host="runcorn.dyndns.org",
-#         user="mpos",
-#         password="mpospass",
-#         database="mpos"
-#     ), mysql.connector.connect(
-#         host="rathbone.dyndns.org",
-#         user="mpos",
-#         password="mpospass",
-#         database="mpos"
-#     ),
-#     mysql.connector.connect(
-#         host="breezehill1.dyndns.org",
-#         user="mpos",
-#         password="mpospass",
-#         database="mpos"
-#     )]
-
 home_db = mysql.connector.connect(
     host="milecross.dyndns.org",
     user="mpos",
     password="mpospass",
     database="mpos"
 )
+
+target_dbs = [
+    "netherly1.dyndns.org",
+    "runcorn.dyndns.org",
+    "rathbone.dyndns.org",
+    "breezehill1.dyndns.org"
+]
 
 
 def rrpextractsummary(mydb):
@@ -54,14 +33,23 @@ def rrpextractsummary(mydb):
 
 
 def dump_original(mydb):
-    cmd = "export MYSQL_PWD=mpospass; mysqldump -h milecross.dyndns.org -u mpos mpos rrpextractsummary --no-tablespaces > summrary_temp.sql"
+    cmd = "export MYSQL_PWD=mpospass; mysqldump -h milecross.dyndns.org -u mpos mpos rrpextractsummary --no-tablespaces > summary_temp.sql"
+    os.system(cmd)
+    cmd = "sed -i 's/utf8mb4_0900_ai_ci/utf8mb4_unicode_ci/' summary_temp.sql"
     os.system(cmd)
 
 
-def extension_pack(mydb):
+def export_to_all(targets):
+    for target in targets:
+        cmd = "export MYSQL_PWD=mpospass; mysql -h {0} -u mpos mpos < summrary_temp.sql".format(str(target))
+        os.system(cmd)
+
+
+def extension_pack(mydb, targets):
     rrpextractsummary(mydb)
     dump_original(mydb)
+    export_to_all(targets)
 
 
 if __name__ == '__main__':
-    extension_pack(home_db)
+    extension_pack(home_db, target_dbs)
