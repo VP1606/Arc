a26163861248bafe0d529693fa293ba1ff06fbb8
VP1606 @ 16MBP <vpremakantha@gmail.com>
2023-08-11 09:14:54 +0100

    Added post-processor.

--- arc_search/extract_search.py
+++ arc_search/extract_search.py
@@ -13,11 +13,9 @@ def search_by_name(mydb: mysql.connector.MySQLConnection, query: str):
     mycursor.execute(query_sql)
 
     rows = mycursor.fetchall()
-    for row in rows:
-        print(row)
-    print(len(rows))
-
     mycursor.close()
+
+    return rows
 
 def search_by_ean(mydb: mysql.connector.MySQLConnection, query: str):
     mycursor = mydb.cursor()
@@ -25,10 +23,52 @@ def search_by_ean(mydb: mysql.connector.MySQLConnection, query: str):
     mycursor.execute(query_sql)
 
     rows = mycursor.fetchall()
-    for row in rows:
-        print(row)
-    print(len(rows))
-
     mycursor.close()
+
+    return rows
+
+def check_sources(mydb: mysql.connector.MySQLConnection, row: list):
+    mycursor = mydb.cursor()
+
+    query = f"SELECT source FROM rrpextract WHERE stockref = {row[0]} AND DATE(datetime) = '{row[3].date()}'"
+    mycursor.execute(query)
+
+    rows = mycursor.fetchall()
+    mycursor.close()
+
+    returner = {
+        'bestway': False,
+        'booker': False
+    }
+
+    if ('BESTWAY', ) in rows:
+        returner['bestway'] = True
+    if ('BOOKER', ) in rows:
+        returner['booker'] = True
+
+    return returner
+
+
+def post_processor(mydb: mysql.connector.MySQLConnection, input_list: list):
+
+    # Remove Duplicate EAN's.
+    unique_lists = {}
+    
+    for sublist in input_list:
+        first_element = sublist[0]
+        if first_element not in unique_lists:
+            unique_lists[first_element] = sublist
+    
+    result = list(unique_lists.values())
+    main_result = list()
+
+    for row in result:
+        sources = check_sources(mydb=mydb, row=row)
+        myrow = list(row)
+        myrow.append(sources)
+        main_result.append(myrow)
+
+    return main_result
+
 
 mydb.close()
