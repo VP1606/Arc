ff99d872a7417fb35ee0a10a2c4e03f67b474a4c
VP1606 @ 16MBP <vpremakantha@gmail.com>
2023-08-15 22:54:23 +0100

    Showing "-" now instead of £0.00.

--- arc_search/static/operator.js
+++ arc_search/static/operator.js
@@ -42,9 +42,14 @@ function jsonHandler(data) {
     if ("status" in bestway) {
         console.log("Error detected in Bestway.")
     } else {
+        var b_price = bestway.wholesale_price;
+        if (b_price == "£0.00") {
+          b_price = "-";
+        };
+
         createRow(
             "Bestway", "OK", bestway.item_name, bestway.ean, bestway.supplier_code, bestway.rsp, 
-            bestway.wholesale_unit_size, bestway.wholesale_price
+            bestway.wholesale_unit_size, b_price
         );
     }
 
