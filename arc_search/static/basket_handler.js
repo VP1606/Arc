81c824ad442f0b5eecbaee8e97cdfa4dfef263a6
VP1606 @ 16MBP <vpremakantha@gmail.com>
2023-08-22 20:30:54 +0100

    Programatically creating items onto UI.

--- arc_search/static/basket_handler.js
+++ arc_search/static/basket_handler.js
@@ -3,10 +3,13 @@ function do_basket_search() {
 
     var loaderUI = document.getElementById("loader-wheel");
     var searchButton = document.getElementById("search-button-clicker");
+    var basketContainer = document.getElementById("basketContainer");
     var noneFound = document.getElementById("noneFoundContainer");
 
     loaderUI.style.display = "block";
     searchButton.style.display = "none";
+    basketContainer.style.display = "none";
+    basketContainer.innerHTML = '';
     noneFound.style.display = "none";
 
     var url = `/operate_basket?id=iahfiasfdosai2313212**7613&key=${encodeURIComponent(key)}`;
@@ -16,6 +19,7 @@ function do_basket_search() {
       })
       .then((response) => response.json())
       .then((json) => {
+
         loaderUI.style.display = "none";
         searchButton.style.display = "block";
 
@@ -24,8 +28,53 @@ function do_basket_search() {
             return;
         };
 
-        console.log(json);
-      });
+        try {
+          json.forEach(item => {
+            basket_createItemCard(item);
+          });
+        }
 
-    
+        catch {
+          noneFound.style.display = "flex";
+          return;
+        };
+
+        basketContainer.style.display = "block";
+      });
+};
+
+function basket_createItemCard(item) {
+  fetch('/static/basket_mode/basket_element.html')
+      .then(response => response.text())
+      .then(data => {
+        data = data.replace("[NAME]", item.name);
+        data = data.replace("[EAN]", item.ean);
+        data = data.replace("[QTY]", item.quantity);
+        data = data.replace("[BEST_SUP]", item.best_supplier);
+
+        data = data.replace("[BW-INSTOCK]", item.bw_instock);
+        data = data.replace("[BW-PCKZ]", item.bw_pack_size);
+        data = data.replace("[BW-UNP]", item.bw_unit_price);
+        data = data.replace("[BW-TLP]", item.bw_total);
+
+        data = data.replace("[BK-INSTOCK]", item.bk_instock);
+        data = data.replace("[BK-PCKZ]", item.bk_pack_size);
+        data = data.replace("[BK-UNP]", item.bk_unit_price);
+        data = data.replace("[BK-TLP]", item.bk_total);
+
+        data = data.replace("[PF-INSTOCK]", item.pf_instock);
+        data = data.replace("[PF-PCKZ]", item.pf_pack_size);
+        data = data.replace("[PF-UNP]", item.pf_unit_price);
+        data = data.replace("[PF-TLP]", item.pf_total);
+
+        var basketContainer = document.getElementById('basketContainer');
+        var newdiv = document.createElement('div');
+        newdiv.innerHTML = data;
+        newdiv.style.paddingBottom = '20px';
+
+        basketContainer.appendChild(newdiv);
+      })
+      .catch(error => {
+        console.error('Error fetching component:', error);
+      });
 };
