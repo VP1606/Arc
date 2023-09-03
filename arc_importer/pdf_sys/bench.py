5dfa9a8aa93f63038cc265401a505419f701707a
VP1606 @ 16MBP <vpremakantha@gmail.com>
2023-09-03 20:27:43 +0100

    Pulling barcodes out of image zones.

--- arc_importer/pdf_sys/bench.py
+++ arc_importer/pdf_sys/bench.py
@@ -1,7 +1,11 @@
 from pdf2image import convert_from_path
 from PIL import Image
+from pyzbar.pyzbar import decode
 
 import image_helper
+
+# export DYLD_LIBRARY_PATH=/opt/homebrew/lib
+# Brew Installed: zbar, poppler
 
 path = "test_35.pdf"
 
@@ -24,3 +28,20 @@ for image in book_images:
 del book_images
 
 print(f"Refined List: {len(refined_book)}")
+
+barcodes_found = list()
+for image in refined_book:
+    barcodes = decode(image)
+    for barcode in barcodes:
+        barcode_data = barcode.data.decode('utf-8')
+        barcode_type = barcode.type
+
+        block = [barcode_data, barcode_type]
+        barcodes_found.append(block)
+        del block
+
+print(f"Barcodes Found: {len(barcodes_found)}")
+
+for block in barcodes_found:
+    data, type = block[0], block[1]
+    print(data, type)
