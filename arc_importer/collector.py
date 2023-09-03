32bf8be68abff7921d790719416634a118970bed
VP1606 @ 16MBP <vpremakantha@gmail.com>
2023-09-03 12:38:42 +0100

    Added IMAP Error Handling.

--- arc_importer/collector.py
+++ arc_importer/collector.py
@@ -5,6 +5,7 @@ import importlib
 import email_handler, csv_handler, config_holder, responder
 
 def get_mail_client(email_address):
+    print("Connecting IMAP...")
     SMTP_SERVER = "imap.gmail.com"
     SMTP_PORT = 993
     
@@ -14,6 +15,7 @@ def get_mail_client(email_address):
 
     mail = imaplib.IMAP4_SSL(SMTP_SERVER)
     mail.login(email_address, password)
+    print("Connected IMAP!")
     return mail
 
 mail = get_mail_client("arcsys.importer@gmail.com")
@@ -26,20 +28,36 @@ while True:
     importlib.reload(config_holder)
     importlib.reload(responder)
     # print("SCAN")
-    mail.select("inbox")
-    result, data = mail.search(None, "UNSEEN")  # Search for unread messages
-    
-    if result == "OK":
-        for num in data[0].split():
-            typ, msg_data = mail.fetch(num, "(RFC822)")
-            if typ == "OK":
-                email_message = email.message_from_bytes(msg_data[0][1])
-                print("New email received:")
-                print("Subject:", email_message["subject"])
-                print("From:", email.utils.parseaddr(email_message["from"])[1])
-                print("----\n")
-                email_handler.handle(message=email_message)
-    else:
-        print("Error searching for emails.")
+
+    try:
+        mail.select("inbox")
+        result, data = mail.search(None, "UNSEEN")  # Search for unread messages
+        
+        if result == "OK":
+            for num in data[0].split():
+                typ, msg_data = mail.fetch(num, "(RFC822)")
+                if typ == "OK":
+                    email_message = email.message_from_bytes(msg_data[0][1])
+                    print("New email received:")
+                    print("Subject:", email_message["subject"])
+                    print("From:", email.utils.parseaddr(email_message["from"])[1])
+                    print("----\n")
+                    email_handler.handle(message=email_message)
+        else:
+            print("Error searching for emails.")
+
+    except imaplib.IMAP4.abort as e:
+        print(f"Caught IMAP4 Abort error! ::: {str(e)}")
+        print("Restarting IMAP Connection...")
+        mail = get_mail_client("arcsys.importer@gmail.com")
+        print("Launched!")
+        print("")
+
+    except Exception as e:
+        print(f"Unexpected error! ::: {str(e)}")
+        print("Restarting IMAP Connection...")
+        mail = get_mail_client("arcsys.importer@gmail.com")
+        print("Launched!")
+        print("")
     
     time.sleep(5)
