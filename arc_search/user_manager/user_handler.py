d96c71df83c95e11ae33ea940a57310bd0433122
VP1606 @ 16MBP <vpremakantha@gmail.com>
2023-09-08 17:48:16 +0100

    Added credential uploading.

--- arc_search/user_manager/user_handler.py
+++ arc_search/user_manager/user_handler.py
@@ -31,3 +31,32 @@ def fetch_users():
     db.close()
 
     return res
+
+
+def upload_login_creds(type, user_id, username, password):
+    db = mysql.connector.connect(
+        host="192.168.1.180",
+        user="mpos",
+        password="mpospass",
+        database="mpos"
+    )
+
+    cursor = db.cursor()
+
+    sql = ""
+    if type == "bw":
+        sql = 'INSERT INTO arc_users_bw_details (user_id, account_number, pw) VALUES (%s, %s, %s);'
+    elif type == "pf":
+        sql = 'INSERT INTO arc_users_pf_details (user_id, username, pw) VALUES (%s, %s, %s);'
+    else:
+        print("UNKNOWN!!!")
+        return
+
+    values = (user_id, username, password)
+
+    cursor.execute(sql, values)
+    db.commit()
+
+    cursor.close()
+    db.close()
+    return
