1284bd523aea7a37c0b3f7fd0176aa910485df4d
VP1606 @ 16MBP <vpremakantha@gmail.com>
2023-09-12 22:26:34 +0100

    Added mode select controller.

---
+++ arc_search/static/UI2/mode_control.js
@@ -0,0 +1,21 @@
+var eanExact = true;
+var nameSearch = false;
+var eanSearch = false;
+var basketMode = false;
+var scanMode = false;
+
+const modeIconBindings = {
+    'exact-ean-btn': ['/static/UI2/img_assets/mode_icons/exact_ean.png', 'Exact EAN', true],
+    'name-search-btn': ['/static/UI2/img_assets/mode_icons/name_search.png', 'Name Search', false],
+    'ean-search-btn': ['/static/UI2/img_assets/mode_icons/ean_search.png', 'EAN Search', false],
+    'basket-btn': ['/static/UI2/img_assets/mode_icons/basket.png', 'Basket', false],
+    'scan-btn': ['/static/UI2/img_assets/mode_icons/scan.png', 'Scan', false]
+};
+function ClickMode(sender) {
+    var main_mode_show = document.getElementById("main_mode_show");
+    const binding = modeIconBindings[sender.id];
+
+    document.getElementById("main_mode_image").src = binding[0];
+    main_mode_show.querySelector('#mode_select_title').textContent = binding[1];
+    modeIconBindings[sender.id][2] = true;
+};
